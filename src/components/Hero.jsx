558e2bfd00f1953cdd8e82a7f3a7a12a9b9ae705
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2023-11-08 19:36:54 -0500

    add: hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="text-white">
+      <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center border">
+        <p className="text-[#00df9a] font-bold p-2">Growing with data</p>
+        <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold">
+          Grow with data.
+        </h1>
+        <div>
+          <p className="md:text-5xl sm:text-4xl text-xl font-bold">
+            Fast, flexible for
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
