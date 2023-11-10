7140c0c0946ffe7d9d3153d2ae6ec074df4efc62
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2023-11-09 20:50:00 -0500

    update hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Typed from "react-typed";
 
 const Hero = () => {
   return (
@@ -8,11 +9,24 @@ const Hero = () => {
         <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold">
           Grow with data.
         </h1>
-        <div>
-          <p className="md:text-5xl sm:text-4xl text-xl font-bold">
+        <div className="flex justify-center items-center">
+          <p className="md:text-5xl sm:text-4xl text-xl font-bold py-4">
             Fast, flexible for
           </p>
+          <Typed
+            className="md:text-5xl sm:text-4xl text-xl font-bold md:pl-4 pl-2"
+            strings={["BTB", "BTC", "SAAS"]}
+            typeSpeed={120}
+            backSpeed={140}
+            loop
+          />
         </div>
+        <p className="md:text-2xl text-xl font-bold text-gray-400">
+          Some bullshit about how you are good and what not
+        </p>
+        <button className="bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3 text-black">
+          Get Started
+        </button>
       </div>
     </div>
   );
