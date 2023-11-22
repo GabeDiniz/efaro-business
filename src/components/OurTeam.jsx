c214652143291b9a97322f73fb9074fd599de739
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2023-11-21 20:26:51 -0500

    Create: mapping function for OurTeam section

--- src/components/OurTeam.jsx
+++ src/components/OurTeam.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 
 const OurTeam = () => {
+  const teamName = ["Eric", "John", "Gabe"];
+
   return (
     <section className="w-full max-w-[1240px] mx-auto">
       <h1 className="md:text-4xl sm:text-3xl text-2xl font-bold py-2 text-[#00df9a]">
         Our Team
       </h1>
+      <div className="grid lg:grid-cols-3">
+        {teamName.map((name) => (
+          <div>
+            <h1>{name}</h1>
+          </div>
+        ))}
+      </div>
       <div className=""></div>
     </section>
   );
