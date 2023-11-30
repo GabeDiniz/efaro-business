1931d5a7846ca4d3abe15141ee3aec7803e04941
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2023-11-29 19:48:54 -0500

    Add: tailwind custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blk: "#303030",
+        base: "#69b4ff",
+        accentLight: "#0085ff",
+        accent: "#CEDFFF",
+        darkCard: "#454545",
+      },
+    },
   },
   plugins: [],
 };
